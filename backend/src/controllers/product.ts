70b31f688d22163ec6dc43366502c997466f1876
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-11-12 20:08:52 +0530

    add product search functionality and random product generation for testing

--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -4,6 +4,10 @@ import { NewProductRequestBody } from "../types/types.js";
 import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
+import {SearchRequestQuery,BaseQueryType} from "../types/types.js"
+import {faker} from "@faker-js/faker"
+
+
 
 export const newProduct=TryCatch(
     async(req:Request<{},{},NewProductRequestBody>,res,next)=>{
@@ -112,3 +116,56 @@ export const deleteProduct=TryCatch(
           })  
     }
 )
+export const getAllProducts=TryCatch(
+    async(req:Request<{},{},{},SearchRequestQuery>,res,next)=>{
+        const {search,price,category,sort}=req.query
+        const page=Number(req.query.page)||1
+        const limit= Number(process.env.PRODUCT_PER_PAGE)||8
+        const skip=limit*(page-1)
+        const baseQuery:BaseQueryType={}
+    if(search){ 
+        baseQuery.name={
+            $regex:search,
+            $options:"i",
+        }
+    }
+    if(price){
+        baseQuery.price={
+            $lte:Number(price)
+        }
+    }
+    if(category){
+       baseQuery.category=category 
+    }
+    const [products,filteredOnlyProducts]=await Promise.all([
+        Product.find(baseQuery).sort(sort && {price:sort==="asc"?1:-1}).limit(limit).skip(skip),
+        Product.find(baseQuery)
+    ])//do baar await bachane aur dono query parallely chalane ke liye 
+        const totalPages=Math.ceil(filteredOnlyProducts.length/limit)
+        return res.status(200).json({
+            success:true,
+            products,
+            totalPages
+        })
+    }
+)
+//genrate random data for testing purpose
+const generateRandomProducts=async(count:number=10)=>{
+    const products=[]
+    for(let i=0;i<count;i++){
+        const product={
+            name:faker.commerce.productName(),
+            photo:"uploads\be9cb719-dea1-4414-bd04-4147a914b266.jpg",
+            price:faker.commerce.price({min:1500,max:80000,dec:0}),
+            stock:faker.commerce.price({min:0,max:100,dec:0}),
+            category:faker.commerce.department(),
+            createdAt:new Date(faker.date.past()),
+            updatedAt:new Date(faker.date.recent()),
+            _v:0,
+        }
+        products.push(product)
+    }
+    await Product.create(products)
+    console.log({success:true})
+}
+generateRandomProducts(40)
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { adminOnly } from "../middlewares/auth.js"
-import { getLatestProducts, newProduct,getAllCategories,getAdminProducts, getProductById, updateProduct, deleteProduct } from "../controllers/product.js";
+import { getLatestProducts, newProduct,getAllCategories,getAdminProducts, getProductById, updateProduct, deleteProduct, getAllProducts } from "../controllers/product.js";
 import { singleUpload } from "../middlewares/multer.js";
 const app=express.Router()
 
@@ -8,6 +8,8 @@ const app=express.Router()
 app.post("/new",adminOnly,singleUpload,newProduct)
 app.get("/latest",getLatestProducts)
 app.get("/categories",getAllCategories)
+app.get("/all",getAllProducts)
 app.get("/admin-products",adminOnly,getAdminProducts)
 app.route("/:id").get(getProductById).put(adminOnly,singleUpload,updateProduct).delete(adminOnly,deleteProduct)
+
 export default app;
