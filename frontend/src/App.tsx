fea489d74f65894b5cbc226a1d9f0e8a0573919f
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-11-20 10:49:07 +0530

    completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,9 @@ import Header from './components/header'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
 import { useDispatch, useSelector } from 'react-redux'
-// @ts-ignore
+
 import { userExist, userNotExist } from './redux/reducer/userReducer'
+// @ts-ignore
 import { getUser } from './redux/api/userAPI'
 import { UserReducerInitialState } from './types/reducer-types'
 import ProtectedRoute from './components/protected-route'
